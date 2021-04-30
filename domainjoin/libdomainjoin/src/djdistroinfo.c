28dfe4431883a1e3f7c09f9dc6bd856cac4701ac
Shivam Samarth <shivamsamarth@gmail.com>
2021-04-30 09:36:00 -0700

    Fix memory leaks observed during domain join/leave

--- domainjoin/libdomainjoin/src/djdistroinfo.c
+++ domainjoin/libdomainjoin/src/djdistroinfo.c
@@ -695,6 +695,7 @@ DJGetLikewiseVersion(
 
     while (TRUE)
     {
+        CT_SAFE_FREE_STRING(line);
         GCE(ceError = CTReadNextLine(versionFile, &line, &isEndOfFile));
         if (isEndOfFile)
             break;
